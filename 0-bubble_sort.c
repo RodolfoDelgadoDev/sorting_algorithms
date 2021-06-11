17683e1a7352316c03d626e8ae1fdab4a508033f
Miguel22247 <minipachru@gmail.com>
2021-06-11 12:43:36 -0300

    added files and modified print files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,10 +8,34 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int aux;
+	int swap;
+	unsigned int i, j;
 
-	if size <= 2
-			   return;
+	if (size <= 2)
+			return;
+	for (i = 0; i <= size; i++)
+	{
+		if (array[i] || array[i + 1])
+		{
+			for (j = 0; j <= size; j++)
+			{
+				if (array[j] || array[j + 1])
+				{
+					if (array[j] > array[j + 1])
+					{
+						swap = array[j];
+						array[j] = array[j + 1];
+						array[j + 1] = swap;
+						print_array(array, size);
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
 
 
 
--- sort.h
+++ sort.h
@@ -1,9 +1,12 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stdlib.h>
 #include <stdio.h>
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 /**
  * struct listint_s - Doubly linked list node
@@ -18,4 +21,5 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
 #endif
