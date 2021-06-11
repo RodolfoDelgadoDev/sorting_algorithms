c449f6b69836602c3e403b66700761a8ad0e62f3
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-11 14:11:39 +0000

    add prototype of ex 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - compares adjacent elements and swaps them
+ * @array: array
+ * @size: size of the array
+ * Return: Void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int aux;
+
+	if size <= 2
+			   return;
+
+
+
+}
