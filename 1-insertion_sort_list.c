a2e84900891541939b9d16789f5fc871115048d0
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-11 22:33:01 +0000

    added more stuff

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,8 @@
 void insertion_sort_list(listint_t **list)
 {
     listint_t *aux = *list;
-    listint_t temp;
+    listint_t *temp;
+    listint_t *ayu;
 
     while (aux)
         {
@@ -18,22 +19,36 @@ void insertion_sort_list(listint_t **list)
                 {
                     temp = aux->next;
                     temp->prev = aux->prev;
-                    aux->prev = temp;
+                    if (aux->prev)
+                        aux->prev = temp;
+                    else
+                        *list = head;
                     if !(temp->next)
-                        aux->next;
+                        aux->next = NULL;
                     else
                     {
                         (temp->next)->prev = aux;
                         (aux->next)->next = temp->next;
                         temp->next = aux;
                     }
+                    print_list(list);
+                    while (temp)
+                    {
+                        if temp->n < (temp->prev)->n
+                        {
+                            ayu = temp->prev;
+                            
 
-
-
+                        }
+                        else
+                            break;
+                    }
                 }
+                else  
+                    aux = aux->next;
             }
-            else
-                aux = aux->next;
+            else  
+                    aux = aux->next;        
         }
     }    
 }
