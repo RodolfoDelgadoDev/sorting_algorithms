fe4324770c86dec6c95a66db0312193b3ca6ae11
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-13 14:15:37 +0000

    added non-functional ex 1, 1-main, and mod. sort.h

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,42 +13,51 @@ void insertion_sort_list(listint_t **list)
 
     while (aux)
         {
-            if aux->next
-            {
-                if aux->n > (aux->next)->n
-                {
-                    temp = aux->next;
-                    temp->prev = aux->prev;
-                    if (aux->prev)
-                        aux->prev = temp;
-                    else
-                        *list = head;
-                    if !(temp->next)
-                        aux->next = NULL;
-                    else
-                    {
-                        (temp->next)->prev = aux;
-                        (aux->next)->next = temp->next;
-                        temp->next = aux;
-                    }
-                    print_list(list);
-                    while (temp)
-                    {
-                        if temp->n < (temp->prev)->n
-                        {
-                            ayu = temp->prev;
-                            
-
-                        }
-                        else
-                            break;
-                    }
-                }
-                else  
-                    aux = aux->next;
-            }
-            else  
-                    aux = aux->next;        
-        }
-    }    
+		if (aux->next)
+		{
+			if (aux->n > (aux->next)->n)
+			{
+				temp = aux->next;
+				temp->prev = aux->prev;
+				if (aux->prev)
+					aux->prev = temp;
+				else
+					*list = aux;
+				if (!(temp->next))
+					aux->next = NULL;
+				else
+				{
+					(temp->next)->prev = aux;
+					(aux->next)->next = temp->next;
+					temp->next = aux;
+				}
+				print_list(*list);
+				while (temp)
+				{
+					if (temp->prev)
+					{
+						if (temp->n < (temp->prev)->n)
+						{
+							ayu = temp->prev;
+							temp->prev = ayu->prev;
+							aux->prev = ayu;
+							temp->next = ayu;
+							if (ayu->prev)
+								(ayu->prev)->next = temp;
+							else
+								*list = temp;
+							ayu->prev = temp;
+						}
+						else
+							break;
+						print_list(*list);
+					}
+					temp = temp->prev;
+				}
+				aux = aux->next;
+			}
+			aux = aux->next;
+		}
+		aux = aux->next;
+	}
 }
