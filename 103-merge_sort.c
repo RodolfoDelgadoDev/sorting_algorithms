22fc94c1c843ce77c9aacc1c2db008f21911bf40
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-15 18:40:01 +0000

    this

---
+++ 103-merge_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * merge_sort - Entry point
+ * @array: array
+ * @size: size
+ * Return: Always 0
+ */
+void merge_sort(int *array, size_t size)
+{
+
+
+
+
+
+}
+/**
+ * auxrecursion - aux function to make recursion
+ * @array: array
+ * @size: size
+ * Return: void
+ */
+void merge_sort(int *array, size_t size)
+{
+
+
+
+
+
+
+
+}
+
+
