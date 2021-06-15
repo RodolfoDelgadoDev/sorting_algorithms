627b55824e3ba6f2537197b3ff20ca0f5945ac5f
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-15 14:08:27 +0000

    ahora?

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *aux = *list, *temp, *ayu;
 
-	if (!(*list) || !list)
+	if ((!(*list) || !list) || !(**list))
 		return;
 	while (aux->next)
 	{
